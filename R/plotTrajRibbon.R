474d3f797f09c1a8cd7f6bfc6e4ccd11e32a2c3a
dmattek <macdobry@gmail.com>
2018-06-13 23:35:36 +0200

    Change default font size

--- R/plotTrajRibbon.R
+++ R/plotTrajRibbon.R
@@ -16,7 +16,7 @@
 #' @param plotlab.arg String with plot title
 #'
 #' @return ggplot2 plot object
-#' @export
+#' @export plotTrajRibbon
 #' @import ggplot2
 #'
 #' @examples
@@ -31,6 +31,26 @@
 #'
 #' # plot each group in a separate facet; add population mean
 #' plotTrajRibbon(dt.arg = dt.aggr, x.arg = 'Metadata_RealTime', y.arg = 'Mean')
+
+myGgplotTheme =
+  theme_bw(base_size = 12, base_family = "Helvetica") +
+  theme(
+    panel.spacing = unit(1, "lines"),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.border = element_blank(),
+    axis.line = element_line(color = "black", size = 0.25),
+    axis.text.x = element_text(size = 12),
+    axis.text.y = element_text(size = 12),
+    axis.title = element_text(size = 14),
+    strip.text = element_text(size = 14, face = "bold"),
+    strip.background = element_blank(),
+    legend.key = element_blank(),
+    legend.text = element_text(size = 12),
+    legend.key.height = unit(1, "lines"),
+    legend.key.width = unit(2, "lines"),
+    legend.position = "right"
+  )
 
 plotTrajRibbon = function(dt.arg,
                           x.arg,
@@ -81,22 +101,8 @@ plotTrajRibbon = function(dt.arg,
   p.tmp = p.tmp +
     xlab(xlab.arg) +
     ylab(ylab.arg) +
-    theme_bw(base_size = 18, base_family = "Helvetica") +
-    theme(
-      panel.grid.minor = element_blank(),
-      panel.grid.major = element_blank(),
-      panel.border = element_blank(),
-      axis.line.x = element_line(color = "black", size = 0.25),
-      axis.line.y = element_line(color = "black", size = 0.25),
-      axis.text.x = element_text(size = 12),
-      axis.text.y = element_text(size = 12),
-      strip.text.x = element_text(size = 14, face = "bold"),
-      strip.text.y = element_text(size = 14, face = "bold"),
-      strip.background = element_blank(),
-      legend.key = element_blank(),
-      legend.key.height = unit(1, "lines"),
-      legend.key.width = unit(2, "lines"),
-      legend.position = legendpos.arg
-    )
+    myGgplotTheme +
+    theme(legend.position = legendpos.arg)
+
   return(p.tmp)
 }
